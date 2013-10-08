24ee44b0be4550ab418e001f8e2e734c4120266c
lakelandcol <thethief45@yahoo.com>
2013-10-08 17:10:52 -0400

    Uploading push.py to git, even though I don't really know what it does

---
+++ push.py
@@ -0,0 +1,6 @@
+# Written By Roger Aiudi
+import os
+msg = str(raw_input("Please enter a commit reason: "))
+os.system("git add .")
+os.system("git commit -m \"{0}\"".format(msg))
+os.system("git push")
