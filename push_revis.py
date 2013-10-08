a76d6c5dbb0170e93f85f86eb5cea2505b74f567
lakelandcol <thethief45@yahoo.com>
2013-10-08 17:20:22 -0400

    My version of push.py, no string conversion is needed when using the raw_input function Roger, it does that on its own

---
+++ push_revis.py
@@ -0,0 +1,13 @@
+# Written By Roger Aiudi
+
+#Revised By Collin Lakeland
+
+import os
+
+msg = raw_input("Please enter a commit reason: ")
+
+os.system("git add .")
+
+os.system("git commit -m \"{0}\"".format(msg))
+
+os.system("git push")
