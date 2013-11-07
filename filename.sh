31c20c02781f7cc7f3b20610e61aa33bf1e77f36
lakelandcol <thethief45@yahoo.com>
2013-11-07 16:36:33 -0500

    organized it better, still need to incorporate argv into it

--- filename.sh
+++ filename.sh
@@ -7,11 +7,8 @@
 # Searches current directory for a given file
 
 echo "What is the filename?"
-
 echo
-
 read filename
-
 if [ ! $filename  ]
 
 # Used ! argument for the Bash builtin "test" (see man page for builtins, under "test" section) 
@@ -19,34 +16,26 @@ if [ ! $filename  ]
 then
     
     echo "Give me a filename!"
-    
     echo
-
     exit
-
 else
-    
     find $filename > /dev/null
-    
     if [ $? = 0 ]
     
     then
 	
 	echo
-
 	echo "The file '$filename' exists and here is what it looks like:"
-    
-	sleep 3s
-    
+	
+	sleep 3s  
 	echo
-    
 	more $filename
-    
     else
-	echo
-    
-	echo 'The given file does not exist in the present working directory.'
     
 	echo
+	echo 'The given file does not exist in the present working directory.'    
+	
+	echo
+    
     fi
 fi
