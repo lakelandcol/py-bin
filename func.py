81ec953e519e3f26038398731cee4b30c58cf072
Collin <thethief45@yahoo.com>
2013-11-22 10:07:19 -0500

    Do cool things

--- func.py
+++ func.py
@@ -2,17 +2,19 @@ class Test:
   def __init__(self):      #self = object name
 	self.x = 4
 	self.y = 5
-  def set_value(self, arg):      #arg = what the function expects for input; self is put in automatically
+  def set_value(self, prop, c):      #arg = an argument = what the function expects for input; self is put in automatically
 	self.x = arg
 	self.z = 1
+	print self.x
 	print arg
   def print_value(self, arg):
 	print self.x
 	self.a = 2
 	self.b = 3
 
-testObject = Test()   #object name = class name #parenthesis means you call the function or class; necessary for object assignment
+testObject = Test()      #SYNTAX:  object name = class name; parenthesis means you call the function or class, this is necessary for object assignment
 testObject.print_value(0)
+print testObject.x
 testObject.set_value(20)
-testObject.print_value()
-print testObject.x, testObject.z, testObject.a, testObject.b
+testObject.print_value(0)
+print testObject.x testObject.z, testObject.a, testObject.b
