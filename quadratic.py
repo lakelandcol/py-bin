ecfab7769f08e114c39b0072250770f2525841fb
lakelandcol <thethief45@yahoo.com>
2013-12-10 23:51:09 -0500

    Finally got the jist of it working; is still buggy though

--- quadratic.py
+++ quadratic.py
@@ -19,34 +19,50 @@ class Quadratic():
 		self.coeffs = sys.argv
 		
 		print len(self.coeffs)
-		if len(self.coeffs) > 4:
-			print "Error: Your equation must be in standard form."
-		elif len(self.coeffs) == 4:
-			self.a = self.coeffs[1]
-			self.b = self.coeffs[2]
-			self.c = self.coeffs[3]
-		elif len(self.coeffs) == 2:
-			self.a = self.coeffs[1]
+		#Try block allows input of the form: 0, b ,0, which gives a ZeroDivisionError
+		try:
+			if len(self.coeffs) > 4:
+				print "Error: Your equation must be of degree 2 or lower and in standard form."
+				sys.exit(0)
+			elif self.coeffs == 4:
+				self.a = int(self.coeffs[1])
+				a = self.a
+				self.b = 0
+				self.c = 0
+			elif len(self.coeffs) == 4:
+				self.a = int(self.coeffs[1])
+				self.b = int(self.coeffs[2])
+				self.c = int(self.coeffs[3])
+				a = self.a
+				b = self.b
+				c = self.c
+			self.x = (-1*b + cmath.sqrt((b**2)-4*a*c))/(2*a)          #First root
+			
+			self.x2 = (-1*b - cmath.sqrt((b**2)-4*a*c))/(2*a)         #Second Root
+		except ZeroDivisionError:
+			self.x = 0
+			self.x2 = 0
+			
+			
+		#This case works.
+		#Input of the form: a
+		if len(self.coeffs) == 2:
+			self.a = int(self.coeffs[1])
+			a = self.a
 			self.b = 0
 			self.c = 0
-		#These assignments make creating the roots easier
-		a = self.a
-		b = self.b
-		c = self.c
+			self.x = cmath.sqrt(0/a)
+			self.x2 = cmath.sqrt(0/a)
+		#These assignments make creating the roots easier.
 		print a, b, c
-		self.x = (-1*b + cmath.sqrt((b**2)-4*a*c))/(2*a)          #First root
-
-		self.x2 = (-1*b - cmath.sqrt((b**2)-4*a*c))/(2*a)         #Second Root
-	#def set_roots(self):
-
-
-
-	def testing(x, x2):
+		
+	#Tests to see what type of solutions there are.
+	def testing(self ,x, x2):
 		if x == x2 and x.imag == 0:
-			print "Your solution is: x=", x.real                    #Double, real root
+			print "Your solution is: x =", x.real                    #Double, real root
 
 		elif x.imag == 0 and x2.imag == 0:                        #Distinct real roots
-			print "Your solutions are: x=", x.real, "and x=", x2.real
+			print "Your solutions are: x =", x.real, "and x =", x2.real
   
 		elif x == x2:
 			print "Your solution is: x = ", x                       #Double, complex root
@@ -55,7 +71,6 @@ class Quadratic():
 			print "Your solutions are: x =", x, "and x =", x2       #Distinct, complex root
 
 solverObject = Quadratic(sys.argv[2:])
-solverObject.set_roots()
 solverObject.testing(solverObject.x, solverObject.x2)
 
 print "Your equation must be in standard form"
@@ -65,56 +80,3 @@ print
 print "Note that j = i = (-1)^1/2"
 
 print
-
-#WARNING *PSUEDOCODE*
-"""
-inputList = they entered stuff here
-a = None
-b = None
-c = None
- 
-if len(inputList) > 3:
-        error and ask them to try again
-else
-        if len(inputList) == 1:
-                a = inputList[0]
-                b = 0
-                c = 0
-        elif len(inputList) == 2:
-                a = inputList[0]
-                b = inputList[1]
-                c = 0
-        else
-                a = inputList[0]
-                b = inputList[1]
-                c = inputList[2]
-"""
-#End of pseudocode
-
-
-#WARNING *Unused code*
-
-#x = cmath.sqrt(a)              #Funny lines
- 
-"""
-
-  if x == x2 and x.imag == 0:
-	print "Your solution is: x=", x.real             #Double, real root
-
-  elif x.imag == 0 and x2.imag == 0:                      #Distinct real roots
-	print "Your solutions are: x=", x.real, "and x=", x2.real
-  
-  elif x == x2:
-	print "Your solution is: x = ", x   #Double, complex root
-    
-  else:
-	print "Your solutions are: x =", x, "and x =", x2    #Distinct, complex root
-"""
-
-
-          #Fix so that input of the form a,0,0 gives correct solution, says solution is 0 now
-
-          #Fix so input 0,0,0 doesn't error
-
-
-
