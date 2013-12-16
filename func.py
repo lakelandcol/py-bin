e6d5a5adafb9d02b2c92224830bc869295e56e9f
lakelandcol <thethief45@yahoo.com>
2013-12-15 20:35:47 -0500

    Refactored, but buggy, fix zero divison error

--- func.py
+++ func.py
@@ -7,18 +7,18 @@ class Test:
 		self.z = 1
 		print c
 	def print_value(self, arg):
-		#print self.x
+		print self.x
 		self.a = 2
 		self.b = 3
 	def variables (self):
 		self.g = 9
 testObject = Test()    #SYNTAX:  object name = class name; parenthesis means you call the function or class, this is necessary for object assignment
 #testObject.print_value(0)
-print testObject.x
+#print testObject.x
 testObject.print_value(1)
-testObject.set_value(20, 1)
-testObject.variables()
-print testObject.x, testObject.z, testObject.a, testObject.b, testObject.g
+#testObject.set_value(20, 1)
+#testObject.variables()
+#print testObject.x, testObject.z, testObject.a, testObject.b, testObject.g
 
 #ASSIGNMENTS:
 #    = *new name* = *old name*
--- quadratic.py
+++ quadratic.py
@@ -6,8 +6,6 @@ from __future__ import division      #division module makes it so the division o
 import cmath                         #cmath module allows for complex solutions
 import sys
 
-#class here
-
 class Quadratic():
   
 	def __init__(self, argv):
@@ -17,48 +15,38 @@ class Quadratic():
 		
 		#user input
 		self.coeffs = sys.argv
-		
 		print len(self.coeffs)
-		
-	def more_than_three_coefficients(self.coeffs):
-		
-		#Try block allows input of the form: 0, b ,0, which gives a ZeroDivisionError
-		try:
+	#Try block allows input of the form: 0, b ,c, which gives a ZeroDivisionError	
+	try:	
+	
+		def more_than_three_coefficients(self):
 			if len(self.coeffs) > 4:
-				print "Error: Your equation must be of degree 2 or lower and in standard form."
+				print "Usage: You must give three coefficents, even if those coefficients are zero, your equation must be of degree 2 or lower, and in standard form."
 				sys.exit(0)
-				
-		def one_coefficient(self.coeffs):
-			elif self.coeffs == 4:
-				self.a = int(self.coeffs[1])
-				a = self.a
-				self.b = 0
-				self.c = 0
-			elif len(self.coeffs) == 4:
+		def less_than_three_coefficient(self):
+			if self.coeffs < 4:
+				print "Usage: You must give three coefficients, even if those coefficents are zero"
+				sys.exit(0)
+		def three_coefficients(self):
+			if len(self.coeffs) == 4:
 				self.a = int(self.coeffs[1])
 				self.b = int(self.coeffs[2])
 				self.c = int(self.coeffs[3])
+				
+				#These assignments make creating the roots easier.
 				a = self.a
 				b = self.b
 				c = self.c
-			self.x = (-1*b + cmath.sqrt((b**2)-4*a*c))/(2*a)          #First root
+				print a, b, c
+							
+				#Root creation
+				self.x = (-1*b + cmath.sqrt((b**2)-4*a*c))/(2*a)        #First root
 			
-			self.x2 = (-1*b - cmath.sqrt((b**2)-4*a*c))/(2*a)         #Second Root
-		except ZeroDivisionError:
-			self.x = 0
-			self.x2 = 0
-			
-			
-		#This case works.
-		#Input of the form: a
-		if len(self.coeffs) == 2:
-			self.a = int(self.coeffs[1])
-			a = self.a
-			self.b = 0
-			self.c = 0
-			self.x = cmath.sqrt(0/a)
-			self.x2 = cmath.sqrt(0/a)
-		#These assignments make creating the roots easier.
+				self.x2 = (-1*b - cmath.sqrt((b**2)-4*a*c))/(2*a)       #Second Root
+	
+	except ZeroDivisionError:
+		self.x = 0
+		self.x2 = 0			
 		print a, b, c
 		
 	#Tests to see what type of solutions there are.
@@ -75,7 +63,8 @@ class Quadratic():
 		else:
 			print "Your solutions are: x =", x, "and x =", x2       #Distinct, complex root
 
-solverObject = Quadratic(sys.argv[2:])
+solverObject = Quadratic(sys.argv[1:])
+solverObject.three_coefficients()       #Must I call every method within the class? What happens when they put in 1 or 2 coefficients?
 solverObject.testing(solverObject.x, solverObject.x2)
 
 print "Your equation must be in standard form"
@@ -85,3 +74,5 @@ print
 print "Note that j = i = (-1)^1/2"
 
 print
+
+#sys.argv[2:]
