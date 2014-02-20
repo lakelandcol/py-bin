630d9cabe6c7b7e8204a60ded2eb68537e5b8390
lakelandcol <thethief45@yahoo.com>
2014-02-20 14:27:26 -0500

    Getting closer to coefficient and power assignment

--- solve.py
+++ solve.py
@@ -3,27 +3,40 @@
 #Program to solve any equation using Newton's Method
 
 from __future__ import division 		#division module makes it so the division operator doesn't round the quotient
-import cmath                    		#cmath module allows for complex solutions
+import cmath                           #cmath module allows for complex solutions
+import sys
 
 class Solver:
 
-	def __init__(self):
+	def __init__(self, argv):
+		x = int()
+		n = int()   #The power
 		print "Your equation must be set equal to zero"
-		#user input
-		equation = raw_input("Input your equation: ")
-		print equation     #Debugging purposes
-		guess = raw_input("Guess the solution:\n")
-		print guess
+		print "Input the coefficients and the corresponding powers of the terms, even if those coefficients or terms are zero"
+		#User input here
+		for arg in sys.argv:
+			coeffs = sys.argv[1+2*x]  #coefficients are odd arguments
+			powers = sys.argv[2+2*x]  #powers are even arguments
+		#This is weird
+		print coeffs
+		print powers
+		#print sys.argv[-1]      #HOW DOES THIS WORK
+		#print equation     #Debugging purposes
+		#guess = raw_input("Guess the solution:\n")
+		#print guess
 	#def derivative(self, equation):
 
 
 	#def Newtons_method(self, equation, guess):
 
-solverObject = Solver()
-"""
-print "Input your equation"
+solverObject = Solver(sys.argv[1:])
 
-**They input it, exactly as is**
+#FIX: HOW TO ALLOW FOR AN INFINITE NUMBER OF COEFFICIENTS AND POWERS, AND ASSIGN THEM UNIQUELY TO A COMMAND ARGUMENT?? MAYBE FOR LOOP??
+"""
+print "Input coefficients and corresponding powers"
+
+
+**They input it them, odd command line arguments being coefficients, even command line arguments being powers"   
 
 store eqation
 
